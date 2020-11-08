e7e551fc9ec65818fd5da3a5b0568efb6e0ca1b3
Arkadiy Nazin <arcan.run@gmail.com>
2020-11-08 20:43:16 +0300

    single product page

--- shop-ui/src/main/java/ru/geekbrains/service/ProductService.java
+++ shop-ui/src/main/java/ru/geekbrains/service/ProductService.java
@@ -1,9 +1,12 @@
 package ru.geekbrains.service;
 
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.dto.response.product.ProductResponse;
+import ru.geekbrains.model.Product;
 import ru.geekbrains.repository.ProductRepository;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,7 +20,14 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<ProductResponse> findAll(){
-       return productRepository.findAll().stream().map(ProductResponse::of).collect(Collectors.toList());
+    public List<ProductResponse> findAll() {
+        return productRepository.findAll().stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
+
+    public ProductResponse findById(Integer id) throws NotFoundException {
+        Product product = productRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Product with id=%d does not exist", id)));
+        return ProductResponse.of(product);
     }
 }
