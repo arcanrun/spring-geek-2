00278614998865d43f838af58ae0fd9d94d70e4d
Arkadiy Nazin <arcan.run@gmail.com>
2020-11-29 21:55:24 +0300

    pic cloud service

---
+++ shop-pic-cloud/src/test/java/ru/geekbrains/shoppiccloud/ShopPicCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.shoppiccloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPicCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
