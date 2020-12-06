d9023a6d8e11d8504713b80e1bd0fb3e31885300
Arkadii Nazin <arcan.run@gmail.com>
2020-12-06 17:14:45 +0300

    integration with brands

--- spring-integration/src/main/java/ru/geekbrains/ImportConfiguration.java
+++ spring-integration/src/main/java/ru/geekbrains/ImportConfiguration.java
@@ -7,14 +7,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.ConsumerEndpointSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.transformer.FileToStringTransformer;
+import org.springframework.integration.jpa.dsl.Jpa;
+import org.springframework.integration.jpa.dsl.JpaUpdatingOutboundEndpointSpec;
+import org.springframework.integration.jpa.support.PersistMode;
 import org.springframework.messaging.MessageHandler;
+import ru.geekbrains.model.Brand;
 
+import javax.persistence.EntityManagerFactory;
 import java.io.File;
 
 @Configuration
@@ -28,8 +34,8 @@ public class ImportConfiguration {
     @Value("${dest.directory.path}")
     private String descDirectoryPath;
 
-//    @Autowired
-//    private EntityManagerFactory entityManagerFactory;
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
 
     @Bean
     public MessageSource<File> sourceDirectory() {
@@ -47,12 +53,12 @@ public class ImportConfiguration {
         return handler;
     }
 
-//    @Bean
-//    public JpaUpdatingOutboundEndpointSpec jpaPersistHandler() {
-//        return Jpa.outboundAdapter(this.entityManagerFactory)
-//                .entityClass(Brand.class)
-//                .persistMode(PersistMode.PERSIST);
-//    }
+    @Bean
+    public JpaUpdatingOutboundEndpointSpec jpaPersistHandler() {
+        return Jpa.outboundAdapter(this.entityManagerFactory)
+                .entityClass(Brand.class)
+                .persistMode(PersistMode.PERSIST);
+    }
 
     @Bean
     public IntegrationFlow fileMoveFlow() {
@@ -60,8 +66,12 @@ public class ImportConfiguration {
                 .filter(msg -> ((File) msg).getName().endsWith(".txt"))
                 .transform(new FileToStringTransformer())
                 //.split(s -> s.delimiters("\n"))
-                .<String, String>transform(String::toUpperCase)
-                .handle(destDirectory())
+                .<String, Brand>transform(name -> {
+                    Brand brand = new Brand();
+                    brand.setName(name);
+                    return brand;
+                })
+                .handle(jpaPersistHandler(), ConsumerEndpointSpec::transactional)
                 .get();
     }
 
